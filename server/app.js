e1d91017134d7468f16205085b00ab82284a4601
Scott DAngelo <scott.dangelo@gmail.com>
2019-05-06 15:34:50 -0600

    Add basic UI stuff.

---
+++ server/app.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var WatsonConversationSetup = require('./lib/watson-conversation-setup');
+var DEFAULT_NAME = 'watson-conversation-ui';
+var fs = require('fs'); // file system for loading JSON
+var vcapServices = require('vcap_services');
+var conversationCredentials = vcapServices.getCredentials('conversation');
+var watson = require('watson-developer-cloud'); // watson sdk
+
+var express = require('express'); // app server
+var bodyParser = require('body-parser'); // parser for post requests
+
+var app = express();
+
+// Bootstrap application settings
+app.use(express.static('./public')); // load UI from public folder
+app.use(bodyParser.json());
+
+var workspaceID; // workspaceID will be set when the workspace is created or validated.
+
+const conversation = new watson.AssistantV1({
+  version: '2018-02-16'
+});
+
+var conversationSetup = new WatsonConversationSetup(conversation);
+var workspaceJson = JSON.parse(fs.readFileSync('data/watson-ui.json'));
+var conversationSetupParams = { default_name: DEFAULT_NAME, workspace_json: workspaceJson };
+conversationSetup.setupConversationWorkspace(conversationSetupParams, (err, data) => {
+  if (err) {
+    console.log(err);
+    //handleSetupError(err);
+  } else {
+    console.log('Assistant is ready!');
+    workspaceID = data;
+  }
+});
+
+// Endpoint to be call from the client side
+app.post('/api/message', function(req, res) {
+
+  if (!workspaceID) {
+    return res.json({
+      output: {
+        text: 'Assistant initialization in progress. Please try again.'
+      }
+    });
+  }
+
+  var payload = {
+    workspace_id: workspaceID,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the conversation service
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+module.exports = app;
